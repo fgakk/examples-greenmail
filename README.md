1fe7d1f7bbcf8efe814e260ed211adeafb86c19b
Fatih Guclu Akkaya <gucluakkaya@gmail.com>
2021-03-06 21:52:35 +0100

    complete test example

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Example of usage of Greenmail within a Junit5 Test
 
 ## Used Frameworks and Libraries
 
+For main application
+
+- **Spring boot** with mail and web starter
+- **Apache Velocity** as template engine for dynamically applying values to html mail templates
+
+For testing
+
+- **Greenmail** for starting mail server and receiving mails for assertions
+- **Jsoup** for extracting mail content in order to assert its elements
+
 ## How to run
 
 This example is a console application. Open project with your favourite editor/IDE and run from the main class
--- src/test/kotlin/net/fgakk/examples/greenmail/ExamplesGreenmailApplicationTests.kt
+++ src/test/kotlin/net/fgakk/examples/greenmail/ExamplesGreenmailApplicationTests.kt
@@ -1,8 +1,10 @@
 package net.fgakk.examples.greenmail
 
 import com.icegreen.greenmail.junit5.GreenMailExtension
+import com.icegreen.greenmail.util.GreenMailUtil
 import com.icegreen.greenmail.util.ServerSetupTest
 import net.fgakk.examples.greenmail.service.MailSenderService
+import org.jsoup.Jsoup
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -19,7 +21,7 @@ class ExamplesGreenmailApplicationTests {
 
         @RegisterExtension
         @JvmField // without this annotation the value is generated  as private and extension cannot be registered
-        var greenMail = GreenMailExtension(ServerSetupTest.SMTP)
+        var greenMail: GreenMailExtension = GreenMailExtension(ServerSetupTest.SMTP).withPerMethodLifecycle(false)
     }
 
     @Autowired
@@ -38,6 +40,20 @@ class ExamplesGreenmailApplicationTests {
     @Test
     @DisplayName("Check mail content")
     fun testMailContent() {
-        // TODO impement the test with jsoup
+
+        mailSenderService.sendMail("Test Subject", "Bob", "bobb@example.net", "This is a test mail")
+
+        val receivedMessages = greenMail.receivedMessages
+        val receivedMessage = receivedMessages[0]
+
+        val doc = Jsoup.parse(GreenMailUtil.getBody(receivedMessage))
+        val link = doc.select("a")[0].attr("href")
+        val pElements = doc.select("p")
+        val greeting = pElements[0].text()
+        val message = pElements[1].text()
+
+        assertEquals("https://github.com/fgakk", link)
+        assertEquals("Hello Bob.", greeting)
+        assertEquals("This is a test mail.", message)
     }
 }
